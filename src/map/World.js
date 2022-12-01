8d390a6b0ed1646decbbfea46f32584d2f910b3d
ThibaultNG <byosoris@gmail.com>
2022-12-01 22:36:53 +0100

    fixing camera to "player" object

--- src/map/World.js
+++ src/map/World.js
@@ -1,11 +1,12 @@
-import { createRoot } from 'react-dom/client'
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls, Stars } from "@react-three/drei";
+import { Canvas, useFrame } from '@react-three/fiber'
+import { PerspectiveCamera, Stars} from "@react-three/drei";
 import { Physics, usePlane, useBox } from "@react-three/cannon";
 import "../styles/style.css";
+import {useRef} from "react";
 
 
 function Box() {
+
     const [ref, api] = useBox(() => ({ mass: 1, position: [0, 2, 0] }));
     return (
         <mesh
@@ -33,15 +34,44 @@ function Plane() {
     );
 }
 
+function Player(){
+    const ref = useRef(null);
+
+    useFrame((state, delta)=>{
+        ref.current.position.x +=0.01;
+    });
+
+    return (
+        <>
+            <group ref={ref}>
+                <PerspectiveCamera
+                    makeDefault
+                    position={[0, 10, 10]}
+                    args={[45, 1.2, 1, 1000]}
+                    rotation={[-Math.PI/6,0,0]}
+                />
+                <mesh castShadow receiveShadow position={[0, 2, 0]}>
+                    <boxBufferGeometry args={[2, 2, 2]} />
+                    <meshStandardMaterial color={"#ff0000"} />
+                </mesh>
+            </group>
+        </>
+    );
+}
+
 export default function World(){
+
+
+
+
+
     return (
         <Canvas>
-            <OrbitControls/>
             <Stars/>
             <ambientLight intensity={0.5}/>
             <spotLight position={[10, 15, 10]} angle={0.3}/>
             <Physics>
-                <Box/>
+                <Player/>
                 <Plane/>
             </Physics>
         </Canvas>
