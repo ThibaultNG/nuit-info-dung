8550138a96d791c8d9e844bb0356f5035e3d869f
ThibaultNG <byosoris@gmail.com>
2022-12-01 18:45:39 +0100

    adding threeJS example

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import World from "./map/World";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <World />
   </React.StrictMode>
 );
 
